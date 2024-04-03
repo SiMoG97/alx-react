2311d63be3620aa85a3297b04867d00d429dabb7
Simo Echaarani <echaaranimohamed@gmail.com>
2024-04-03 20:03:14 +0000

    task_1 0x01 react intro

---
+++ 0x01-react_intro/task_1/dashboard/src/Notification.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Notification.css";
+
+export function Notification() {
+  return <div className="Notification">Here is the list of notifications</div>;
+}
---
+++ 0x01-react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,10 @@
+export function getFullYeat() {
+  return new Date().getFullYear();
+}
+
+export function getFooterCopy(isIndex) {
+  if (isIndex === true) {
+    return "Holberton School";
+  }
+  return "Holberton School main dashboard";
+}
