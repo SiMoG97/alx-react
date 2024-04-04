35107a234de5b2bba4e66ea590c82d92500d81e1
Simo Echaarani <echaaranimohamed@gmail.com>
2024-04-04 22:59:35 +0000

    asdfas

--- 0x01-react_intro/task_4/dashboard/src/App.js
+++ 0x01-react_intro/task_4/dashboard/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>School dashboards</h1>
+        <h1>School dashboard</h1>
       </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
