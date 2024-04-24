9196620f1c3c451eb50b625b9e1567dcffedd028
Simo Echaarani <echaaranimohamed@gmail.com>
2024-04-24 15:35:17 +0100

    0-fromjs.js

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,16 +1,5 @@
-const { fromJS } = require("immutable");
+import { fromJS } from "immutable";
 
-function getImmutableObject(object) {
+export default function getImmutableObject(object) {
   return fromJS(object);
 }
-
-// console.log(
-//   getImmutableObject({
-//     fear: true,
-//     smell: -1033575916.9145899,
-//     wall: false,
-//     thing: -914767132,
-//   })
-// );
-
-exports = getImmutableObject;
