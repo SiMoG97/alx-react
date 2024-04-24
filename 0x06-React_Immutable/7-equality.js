4032be252da014b4b5fd73eda4ace36da609e3ed
Simo Echaarani <echaaranimohamed@gmail.com>
2024-04-24 17:27:09 +0100

    7-equality.js

---
+++ 0x06-React_Immutable/7-equality.js
@@ -0,0 +1,14 @@
+export default function areMapsEqual(map1, map2) {
+  return map1.equals(map2);
+}
+
+// const map1 = new Map({
+//   firstName: 'Guillaume',
+//   lastName: 'Salva',
+// });
+// const map2 = new Map({
+//   firstName: 'Guillaume',
+//   lastName: 'Salva',
+// });
+
+// console.log(areMapsEqual(map1, map2));
