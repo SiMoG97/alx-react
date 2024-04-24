a5c2fbeab3d419945536b2e8932eac0054dc9e16
Simo Echaarani <echaaranimohamed@gmail.com>
2024-04-24 17:17:45 +0100

    6-deeply.js

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,30 +1,5 @@
 import { Map } from 'immutable';
 
-export function mergeDeeplyElements(page1, page2) {
+export default function mergeDeeplyElements(page1, page2) {
   return Map(page1).mergeDeep(Map(page2));
 }
-
-// // tests
-// const page1 = {
-//   'user-1': {
-//     id: 1,
-//     name: 'test',
-//     likes: {
-//       1: {
-//         uid: 1234,
-//       },
-//     },
-//   },
-// };
-
-// const page2 = {
-//   'user-1': {
-//     likes: {
-//       2: {
-//         uid: 134,
-//       },
-//     },
-//   },
-// };
-
-// console.log(mergeDeeplyElements(page1, page2).toJSON());
