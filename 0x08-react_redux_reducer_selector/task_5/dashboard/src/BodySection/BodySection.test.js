f4d5b800f89be5f883e5e9a774409e205b24bd11
Simo Echaarani <echaaranimohamed@gmail.com>
2024-05-08 16:57:05 +0100

    0x08 task_5

---
+++ 0x08-react_redux_reducer_selector/task_5/dashboard/src/BodySection/BodySection.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { shallow } from "enzyme";
+import BodySection from "./BodySection";
+import { StyleSheetTestUtils } from "aphrodite";
+
+StyleSheetTestUtils.suppressStyleInjection();
+
+describe("", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(
+      <BodySection title="test title">
+        <p>test children node</p>
+      </BodySection>
+    );
+  });
+
+  it("Checks that shallowing the component should render correctly the children and one h2 element", () => {
+    const paragraph = wrapper.find("p");
+    expect(paragraph.length).toEqual(1);
+    expect(paragraph.text()).toEqual("test children node");
+
+    const h2Title = wrapper.find("h2");
+    expect(h2Title.length).toEqual(1);
+    expect(h2Title.text()).toEqual("test title");
+  });
+});
---
+++ 0x08-react_redux_reducer_selector/task_5/dashboard/src/HOC/WithLogging.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import WidthLogging from "./WithLogging";
+import { mount } from "enzyme";
+import Login from "../Login/Login";
+import { StyleSheetTestUtils } from "aphrodite";
+
+StyleSheetTestUtils.suppressStyleInjection();
+
+describe("WithLogging() HOC testing Component life-cycle", () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it("WithLogging componentDidMount tests", () => {
+    const consoleLog = jest.spyOn(console, "log").mockImplementation();
+
+    const WithLoggingLogin = WidthLogging(() => <Login />);
+    const wrapper = mount(<WithLoggingLogin />);
+
+    expect(consoleLog).toHaveBeenCalledWith("Component Login is mounted");
+    wrapper.unmount();
+    expect(consoleLog).toHaveBeenCalledWith(
+      "Component Login is going to unmount"
+    );
+  });
+});
